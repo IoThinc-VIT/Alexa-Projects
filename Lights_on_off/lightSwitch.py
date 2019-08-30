466ba6499303af375fa38c14016b4f1176752dcf
nobi1007 <mittalshyam1007@gmail.com>
2019-08-30 15:03:14 +0530

    automate home added

---
+++ Lights_on_off/lightSwitch.py
@@ -0,0 +1,59 @@
+from flask import Flask
+
+from flask_ask import Ask, statement, question, session
+
+from lightsTemplate import *
+
+app = Flask(__name__)
+
+ask = Ask(app, "/")
+
+lastStatus = ""
+
+@app.route("/")
+def starting():
+    return "<span style = color:blue>These are template lines:</span><br>--" + "<br>--".join(d.values())
+
+@ask.launch
+
+def launch_skill():
+
+    welcome_msg = d["welcome"]
+    return question(welcome_msg)
+
+@ask.intent("describeIntent")
+
+def description():
+    about = d["about_the_skill"]
+    return question(about)
+
+@ask.intent("switchingLights", convert={'key':str})
+
+def switch_light(key):
+    response = ''
+    global lastStatus
+    if key == "on" or key == "off":
+        if lastStatus == key:
+            response = "The light is already turned {0}".format(key)
+        else:
+            lastStatus = key
+            response = d["switch_on_off_template"].format(key)
+    else:
+        response = d["invalid_switch"]
+    return question(response)
+
+@ask.intent("AMAZON.HelpIntent")
+
+def switch_help():
+    response = d["help_template"]
+    return question(response)
+
+@ask.intent("AMAZON.StopIntent")
+
+def switch_stop():
+    response = d["stop_template"]
+    return statement(response)
+
+if __name__ == '__main__':
+
+    app.run(port=8000,debug=True)
---
+++ Lights_on_off/lightsTemplate.py
@@ -0,0 +1,12 @@
+d = {}
+d["welcome"] = "Welcome to the Automate Home Skill. Using this skill onw can toggle the lights. You wanna try it out? Just start by saying... turn on the light."
+
+d["help_template"] = "To begin, you can say... turn on the light!"
+
+d["stop_template"] = "Thank you for using the Automate Home skill! We hope you enjoyed the experience."
+
+d["invalid_switch"] = "Sorry, the given switch condition is not valid."
+
+d["switch_on_off_template"] = "Turning {0} the light."
+
+d["about_the_skill"] = "Automate home is a very basic skill which can be utilized to turning on or off the lights of the home. To begin with, try saying... turn on the light."
